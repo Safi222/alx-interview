5754342a450264d9d23c685ecc8eed6ec5b8319b
Safi222 <safia2000gibril@gmail.com>
2024-12-14 02:58:06 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,19 +20,20 @@ def isWinner(x, nums):
     """choose the winner of the prime game either Maria or Ben"""
     if not x or not numbers or x < 0:
         return None
+
     Maria_wins = 0
     Ben_wins = 0
     the_max = max(numbers[0:x])
-    primes_lists = SieveOfEratosthenes(the_max)
+    primes_arr = SieveOfEratosthenes(the_max)
 
     for i in range(x):
-        prime_nums = primes_lists[numbers[i]]
+        prime_nums = primes_arr[numbers[i]]
         if (prime_nums % 2):
             Maria_wins += 1
         else:
             Ben_wins += 1
 
-        if (Maria_wins > Ben_wins):
-            return 'Maria'
-        if (Ben_wins > Maria_wins):
-            return 'Ben'
+    if (Maria_wins > Ben_wins):
+        return 'Maria'
+    if (Ben_wins > Maria_wins):
+        return 'Ben'
